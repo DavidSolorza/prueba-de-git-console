01db0911c3f0f4892ead1a678231d82f774cd4ae
Luisa Ibarra <ibarratucano@gmail.com>
2025-02-16 13:28:03 -0500

    Figuras

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print ("Me alegra mucho mano")
 print ("✌️")
 print ("Ella esta en parcial de macro enconimia ")
 print ("Hola muchachos, parcial en el que me fue pesimo")
+
+print ("Cuadrado")
+lado = 5
+for _ in range(lado):
+    print("* " * lado)
+
+print("Rectangulo")
+for i in range(1, lado + 1):
+    print(" " * (lado - i) + "*" * (2 * i - 1))
